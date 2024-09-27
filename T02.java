1ed56049462b79c7df2317f7a2e20bb32b9a6779
RospikaSiregar <rospikasarahsiregar@gmail.com>
2024-09-27 10:45:16 +0700

    Kami telah menyelesaikan tugas praktikum T02

--- T02.java
+++ T02.java
@@ -1,1 +1,47 @@
-// NIM - Your Name
+// 12S24008 - Rospika Sarah Yosefin Siregar
+// 12S24013 - Adryan Julianto Panjaitan
+
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judulbuku, penulis, kategori, formatbukuelektronik, penerbit;
+        int tahunterbit, stokbuku;
+        double hargapembelian, minimummargin, rating;
+
+        iSBN = input.nextLine();
+        judulbuku = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatbukuelektronik = input.nextLine();
+        hargapembelian = input.nextDouble();
+        minimummargin = input.nextDouble();
+        stokbuku = input.nextInt();
+        rating = input.nextDouble();
+        if (rating >= 4.7 && rating <= 5.0) {
+            kategori = "Best Pick";
+        } else {
+            if (rating >= 4.5 && rating < 4.7) {
+                kategori = "Must Read";
+            } else {
+                if (rating >= 4.0 && rating < 4.5) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0 && rating < 4.0) {
+                        kategori = "Average";
+                    } else {
+                        if (rating < 3.0 && rating >= 0) {
+                            kategori = "Low";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judulbuku + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargapembelian + "|" + minimummargin + "|" + stokbuku + "|" + rating + "|" + kategori);
+    }
+}
+
